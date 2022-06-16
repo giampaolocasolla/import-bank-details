328aaa1d9c12e40d932b477c09fe9986a6557301
Giampaolo Casolla <giampaolocasolla@gmail.com>
2022-06-16 21:14:43 +0200

    Complete first version

---
+++ main.py
@@ -0,0 +1,45 @@
+def main():
+    import os
+    import glob
+    import yaml
+    import pandas as pd
+
+    cwd = os.getcwd()
+
+    with open("config.yaml", 'r', encoding='utf-8') as stream:
+        config = yaml.safe_load(stream)
+
+    folders_data = os.listdir('data')
+    file_data = {folder: max(glob.glob(os.path.join(cwd, 'data', folder, '*')), key=os.path.getctime) for folder in folders_data}
+
+    df = None
+
+    for file_key, file_value in file_data.items():
+        if config[file_key]['import']:
+            df_temp = pd.read_csv(file_value, **config[file_key]['import'])
+        else:
+            df_temp = pd.read_csv(file_value)
+        
+        if 'Bank' in config[file_key]['columns_old']:
+            df_temp['Bank'] = file_key
+        df_temp = df_temp[config[file_key]['columns_old']]
+        df_temp = df_temp.rename(columns=dict(zip(config[file_key]['columns_old'], config[file_key]['columns_new'])))
+        
+        try:
+            df_temp = df_temp[~df_temp['Expense name'].str.contains('|'.join(config[file_key]['Remove']))]
+        except KeyError:
+            pass
+
+        df_temp['Day'] = pd.to_datetime(df_temp['Day'], format=config[file_key]['Day'])
+
+        
+        df = pd.concat([df, df_temp], ignore_index=True)
+    df['Day'] = df['Day'].dt.round('D')
+
+    df = df.sort_values(by=['Day', 'Expense name', 'Amount'])
+
+    filename = f"{df['Day'].max().strftime('%Y-%m-%d')}_{'-'.join(config.keys())}.xlsx"
+    df.to_excel(os.path.join('output', filename), index=False)
+
+if __name__ == '__main__':
+    main()
